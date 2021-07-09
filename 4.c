f4d4fda337651796f254fe2fbf76b6ec2523b50f
miroj-lovesmomo <81049151+miroj-lovesmomo@users.noreply.github.com>
2021-07-09 09:22:24 +0545

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+struct player{
+char name[20];
+int age, mp, ar;
+};
+
+void main()
+{
+    int i,j,temp,temp1, temp2,n=2;
+    char chtemp[20];
+    struct player info[10];
+    for(i=0; i<n; i++)
+    {
+        printf("for player number %d \n", i+1);
+        printf("enter the name: ");
+        scanf("%s", info[i].name);
+        printf("age: ");
+        scanf("%d",&info[i].age);
+        printf("matches played: ");
+        scanf("%d",  &info[i].mp);
+        printf("average runs: ");
+        scanf("%d", & info[i].ar);
+        printf("\n");
+    }
+   printf("\nthe informations of the players are: ");
+    printf("\nname \t\tage \t\t\tmatch played \t\tavg runs");
+    for(i=0; i<n ;i++)
+    {
+        printf("\n%s\t\t%d \t\t\t%d \t\t\t%d", info[i].name,info[i].age, info[i].mp,info[i].ar);
+    }
+
+//sorted order
+
+printf("\nsorted oeder by the average runs");
+for(i=0;i<=n;i++)
+{
+    for(j=i+1; j<n; j++)
+    {
+        if(info[i].ar>info[j].ar)
+        {
+
+            temp = info[i].ar;
+            temp1 = info[i].age;
+            temp2 = info[i].mp;
+            strcpy(chtemp,info[i].name);
+            info[i].ar = info[j].ar;
+            info[i].age = info[j].age;
+            info[i].mp = info[j].mp;
+            strcpy(info[i].name,info[j].name);
+            info[j].ar = temp;
+            info[j].age= temp1;
+            info[j].mp = temp2;
+            strcpy(info[j].name,chtemp);
+
+        }
+    }
+}
+ printf("\nthe informations of the engines are: ");
+    printf("\nname \t\tage \t\t\tmatch played \t\tavg runs");
+    for(i=0; i<n ;i++)
+    {
+        printf("\n%s\t\t%d \t\t\t%d \t\t\t%d", info[i].name,info[i].age, info[i].mp,info[i].ar);
+    }
+
+}
+
+
